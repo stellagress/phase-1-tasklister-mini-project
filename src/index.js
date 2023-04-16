e8b0d7c6b51bd94ba80b8bc5a2b21b0919d43789
Stella Mazzillo <stellagress5@gmail.com>
2023-04-15 21:47:55 -0400

    complete challenge tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,184 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  addingEventListeners()
+})
+
+// get the form and add an event listener
+function addingEventListeners() {
+  document
+     .getElementById("create-task-form")
+     .addEventListener("submit", handleFormSubmit)
+}
+
+function handleFormSubmit(e) {
+   e.preventDefault()
+   console.log(e)
+   const task = e.target[0].value
+   const priorityLevel = parseInt(e.target.priority.value)
+   
+   displayTask(task, priorityLevel)
+
+   e.target.reset()
+}
+
+function displayTask(task, priorityLevel) {
+   const taskUl = document.getElementById("tasks")
+   const taskLi = document.createElement("li")
+   const deleteBtn = document.createElement("button")
+
+   deleteBtn.textContent = "x"
+   deleteBtn.addEventListener("click", deleteTask)
+
+   taskLi.textContent = task + " "
+   taskLi.style.color = getPriorityColor(priorityLevel)
+   taskLi.appendChild(deleteBtn)
+   taskUl.appendChild(taskLi)
+}
+
+function deleteTask(e) {
+   console.log(e)
+   e.target.parentNode.remove()
+}
+
+function getPriorityColor(priorityLevel) {
+   if (priorityLevel === 1) {
+       return "red"
+   } else if (priorityLevel === 2) {
+       return "blue"
+   } else {
+       return "green"
+   }
+}
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+//   addingEventListeners()
+
+
+// });
+
+
+// function addingEventListeners() {
+//   document.getElementById("create-task-form").addEventListener("submit", handleFormSubmit)
+// }
+
+// function handleFormSubmit (e) {
+//     e.preventDefault()
+
+//     const task = e.target[0].value
+//     const priorityLevel = parseInt(e.target.priority.value)
+
+//     displayTask(task, priorityLevel)
+
+//     e.target.reset()
+
+// }
+
+// function displayTask(tasks, priorityLevel){
+//     const taskUL = document.getElementById("tasks")
+//     const taskLi = document.createElement("li")
+//     const deleteBtn = document.createElement("button")
+
+//     deleteBtn.textContent = "X"
+//     deleteBtn.addEventListener("click", deleteTask)
+
+//     taskLi.textContent = tasks + " "
+//     taskLi.style.color = getPriorityLevel(priorityLevel)
+//     taskLi.appendChild(deleteBtn)
+//     taskUL.appendChild(taskLi)
+
+// }
+
+// function deleteTask (e) {
+//     e.target.parentNode.remove()
+
+// }
+
+
+
+// function getPriorityLevel (priorityLevel) {
+//     if(priorityLevel === 1) {
+//         return "red"
+//     } else if (priorityLevel === 2) {
+//         return "blue"
+//     } else {
+//         return "green"
+//     }
+// }
+
+
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+//   addingEventListeners()
+
+
+// });
+
+
+// function addingEventListeners() {
+//   document.getElementById("#create-task-form").addEventListener("submit", handleFormSubmit)
+// }
+
+// function handleFormSubmit (e) {
+//     e.preventDefault()
+
+//     const task = e.target["new-task-descirption"].value
+//     const priorityLevel = parseInt(e.target.priority.value)
+
+//     displayTask(task, priorityLevel)
+
+// }
+
+// function displayTask(tasks, priorityLevel){
+//     const taskUL = document.getElementById("#tasks")
+//     const taskLi = document.createElement("li")
+//     const deleteBtn = document.createElement("button")
+
+//     deleteBtn.textContent = "X"
+//     deleteBtn.addEventListener("click", deleteTask)
+
+//     taskLi.textContent = tasks + " "
+//     taskLi.style.color = getPriorityLevel(priorityLevel)
+//     taskLi.appendChild(deleteBtn)
+//     taskUL.appendChild(taskLi)
+
+// }
+
+// function deleteTask (e) {
+//     e.target.parentNode.remove()
+
+// }
+
+
+
+// function getPriorityLevel (priorityLevel) {
+//     if(priorityLevel === 1) {
+//         return "red"
+//     } else if (priorityLevel === 2) {
+//         return "blue"
+//     } else {
+//         return "green"
+//     }
+// }
+
+
+
+
+
+
+
